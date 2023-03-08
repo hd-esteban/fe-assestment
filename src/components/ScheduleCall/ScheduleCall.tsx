acb0a37ce06974c7798378b78683d59c62cd5b76
Esteban Hernández <estebanhd@Estebans-MacBook-Pro-2.local>
2023-03-07 21:52:47 -0600

    Remove key

--- src/components/ScheduleCall/ScheduleCall.tsx
+++ src/components/ScheduleCall/ScheduleCall.tsx
@@ -4,7 +4,7 @@ function ScheduleCall() {
   return (
   <div className="flex flex-col items-center justify-center my-12">
     <p className="my-10 text-center">Unsatisfied with these stats? We can help you improve them.</p>
-    <Button key="schedule-call-btn" className="w-11/12 md:w-44" text="Schedule A Call" />
+    <Button className="w-11/12 md:w-44" text="Schedule A Call" />
   </div> );
 }
 
